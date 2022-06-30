cfef814e65a750714faf1cb1ee348569f6c5f600
Rolit Trivedi <rolit454@gmail.com>
2022-06-30 19:19:58 +0530

    nothing

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SearchContextProvider>
-     <App />
+      <App/>
     </SearchContextProvider>
   </React.StrictMode>
 );
