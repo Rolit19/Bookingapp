ef5858382da5671066a56d585f4ecf12662082a0
Rolit Trivedi <rolit454@gmail.com>
2022-07-05 23:14:51 +0530

    changes

--- api/controller/hotel.js
+++ api/controller/hotel.js
@@ -52,7 +52,7 @@ export const getHotels = async (req, res, next) => {
   try {
     const hotels = await Hotel.find({
       ...others,
-      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      cheapestPrice: { $gt: min | 1, $lt: max || 100000 },
     }).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -5,7 +5,7 @@ import './featured.css';
 
 const Featured = () =>{
 
-    const {data,loading,error} = useFetch("http://localhost:8800/api/hotels/countByCity?cities=berlin,madrid,london");
+    const {data,loading,error} = useFetch("http://localhost:8800/api/hotels/countByCity?cities=Berlin,Madrid,London");
     // console.log(data);
 
     return(
